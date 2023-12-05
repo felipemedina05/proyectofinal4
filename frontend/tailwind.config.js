730926cad7aabd4f699d653010e210baea149258
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-12-05 11:29:28 -0500

    login

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/jsx/**/*.jsx",
-  ],
+  content: [ './src/**/*.{js,jsx,ts,tsx}',
+  './index.html',],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")],
+  plugins: [],
 }
